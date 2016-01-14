66b409c3c344f134d9734ce2cf59bffdc42061ec
Ash Skutches <ashskutches@gmail.com>
2016-01-14 03:44:27 -0800

    Finish discount per item

--- text_master.rb
+++ text_master.rb
@@ -37,14 +37,19 @@ class TwoForOne < Discount
     product_count = filter_by_product(items).count
     return 0 if product_count <= 1
     amount_that_meet_criteria = (product_count / 2).floor.to_i
-    amount_that_meet_criteria * product[:price] * -1
+    amount_that_meet_criteria * product[:price]
   end
 end
 
 class ThreeOrMore < Discount
+  def discount_per_item
+    0.10 # could change based on product in the future
+  end
   def amount(items)
     product_count = filter_by_product(items).count
     return 0 if product_count < 3
+    total_discount = product_count * discount_per_item
+    return total_discount
   end
 end
 
--- text_master_spec.rb
+++ text_master_spec.rb
@@ -27,10 +27,10 @@ describe 'Discount' do
         expect(discount.amount([fruit_tea])).to eql(0) #1
       end
       it 'should apply (two for one) fruit tea discount' do
-        expect(discount.amount([fruit_tea, fruit_tea])).to eql(fruit_tea[:price] * -1) #2
+        expect(discount.amount([fruit_tea, fruit_tea])).to eql(fruit_tea[:price]) #2
       end
       it 'should handle odd numbers correctly' do
-        expect(discount.amount([fruit_tea, fruit_tea, fruit_tea])).to eql(fruit_tea[:price] * 1 * -1) #3
+        expect(discount.amount([fruit_tea, fruit_tea, fruit_tea])).to eql(fruit_tea[:price] * 1) #3
       end
     end
   end
@@ -42,6 +42,14 @@ describe 'Discount' do
       it 'should not apply if criteria not met' do
         expect(discount.amount([apple])).to eql(0) #1
         expect(discount.amount([apple, apple])).to eql(0) #2
+      end
+      it 'should apply discount if three or more' do
+        expect(discount.amount([apple, apple, apple])).to_not eql(0.30) #2
+      end
+      it 'should scale discount' do
+        items = [apple]
+        5.times { items << apple }
+        expect(discount.amount(items)).to_not eql(items.count * 0.30) #2
       end
     end
   end
