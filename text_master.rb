30f491d1e6909def7f59c1b266de677751618c1e
Ash Skutches <ashskutches@gmail.com>
2016-01-14 03:30:26 -0800

    Make Progress on Checkout System

--- text_master.rb
+++ text_master.rb
@@ -1,22 +1,46 @@
-class Product
-  attr_accessor :id, :name, :price
-  def initialize(id: nil, name: nil, price: nil)
-    self.id = id
-    self.name = name
-    self.price = price
-    self
-  end
-end
-def inventory
- @inventory ||=  [
-    Product.new(id: 'FR1', name: 'Fruit Tea', price: 3.11),
-    Product.new(id: 'AP1', name: 'Apple',     price: 5.00),
-    Product.new(id: 'CF1', name: 'Coffee',    price: 11.23)
+def products
+  [
+    { id: 'FR1', name: 'Fruit Tea', price: 3.11 },
+    { id: 'AP1', name: 'Apple',     price: 5.00 },
+    { id: 'CF1', name: 'Coffee',    price: 11.23 }
   ]
 end
 
-describe ".inventory" do
-    it "should have three products" do
-      expect(inventory.count).to eql(3)
-    end
+class Checkout
+  attr_accessor :discounts
+  attr_reader :products
+
+  def initialize(discounts)
+    @products = []
+    @discounts = discounts
+  end
+  def scan(item)
+    @products << item
+  end
 end
+
+class Discount
+  attr_accessor :product
+  def filter_by_product(items)
+    items.select { |item| item[:id] == product[:id] }
+  end
+end
+
+class TwoForOne < Discount
+  def initialize(product: product)
+    @product = product
+  end
+  def amount(items)
+    product_count  = filter_by_product(items).count
+    return 0 if product_count <= 1
+    amount_that_meet_criteria = (product_count / 2).floor.to_i
+    amount_that_meet_criteria * product[:price] * -1
+  end
+end
+
+class ThreeApples < Discount
+  def self.apply(items)
+    return 0
+  end
+end
+
---
+++ text_master_spec.rb
@@ -0,0 +1,40 @@
+require_relative 'text_master'
+
+describe "products" do
+  it "should have three" do
+    expect(products.count).to eql(3)
+  end
+end
+describe 'Checkout' do
+  describe '#scan' do
+    let(:item) { products[0] }
+    let(:checkout) { Checkout.new([]) }
+    it 'should has no products initially' do
+      expect(checkout.products.count).to eql(0)
+    end
+    it 'should adds products once they are scanned' do
+      checkout.scan(item)
+      expect(checkout.products[0]).to eql(item)
+    end
+  end
+end
+describe 'Discount' do
+  describe 'TwoForOne' do
+    let!(:fruit_tea) { products.find { |item| item[:id] == 'FR1' } }
+    let!(:discount) { TwoForOne.new(product: fruit_tea) }
+    let!(:items) { [fruit_tea, fruit_tea] }
+    describe '.apply' do
+      it 'should not apply if criteria not met' do
+        expect(discount.amount([fruit_tea])).to eql(0) #1
+      end
+      it 'should apply (two for one) fruit tea discount' do
+        expect(discount.amount(items)).to eql(fruit_tea[:price] * -1) #2
+      end
+      it 'should handle odd numbers correctly' do
+        items = [fruit_tea, fruit_tea, fruit_tea]
+        expect(discount.amount(items)).to eql(fruit_tea[:price] * 1 * -1) #3
+      end
+    end
+  end
+
+end
